013b66fc4632265945f670a1500a0253790a4893
Yavuz Selim Yiğit <yavuzselimyigit06@hotmail.com>
2025-06-12 01:05:53 +0300

    02-carpma-islemi

---
+++ 02-carpma-islemi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Kullanıcıdan 2 adet int sayı alınız ve çarpınız.
+sonucu ekrana yazdırınız.
+*/
+
+
+namespace _02_carpma_islemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2, carpma;
+            Console.Write("Lütfen birinci sayıyı giriniz: ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            // Convert.ToInt32, kullanıcıdan alınan string değeri int'e çevirir.
+
+            Console.Write("Lütfen ikinci sayıyı giriniz: ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            carpma = sayi1 * sayi2;
+
+            Console.WriteLine($"Çarpım sonucu: {carpma}");
+        }
+    }
+}
---
+++ 02-carpma-islemi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("02-carpma-islemi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02-carpma-islemi")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("245e5125-293a-414c-a3c6-7659e69cd88e")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
